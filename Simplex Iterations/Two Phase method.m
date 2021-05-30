aa5bed9413e838c6703cfbcd8c233e252e932494
RIthik Chaudhary <71806874+Rithik57@users.noreply.github.com>
2021-05-30 21:38:19 +0530

    Update Two Phase method.m

--- Simplex Iterations/Two Phase method.m
+++ Simplex Iterations/Two Phase method.m
@@ -27,56 +27,56 @@ initialTable.Properties.VariableNames(1:size(A,2))=variables
 
 RUN = true;
 while RUN
-%find the most negative value excluding the sol column
-ZC = ZjCj(1,1:end-1);
-if any(ZC<0)
-    
-    %find entering variable
-    [EnterCol pvt_col] = min(ZC);  %most negative value and pivot column index
-    fprintf('pivot column is : %d\n',pvt_col);
-    %find leaving variable
-    %find ratio of last column and pivot column values
-    sol = A(:,end); %last column
-    column = A(:,pvt_col); %pivot column
-    %if all pivot column values are negative then unbounded solution
-    if column<0
-        fprintf('Unbounded solution\n');
-    else
-        for i=1:size(A,1) %for every value in pivot column
-            if column(i)>0 %if positive then get ratio
-                ratio(i) = sol(i)./column(i);
-            else  %else store as inf
-                ratio(i) = inf;
+    %find the most negative value excluding the sol column
+    ZC = ZjCj(1,1:end-1);
+    if any(ZC<0)
+
+        %find entering variable
+        [EnterCol pvt_col] = min(ZC);  %most negative value and pivot column index
+        fprintf('pivot column is : %d\n',pvt_col);
+        %find leaving variable
+        %find ratio of last column and pivot column values
+        sol = A(:,end); %last column
+        column = A(:,pvt_col); %pivot column
+        %if all pivot column values are negative then unbounded solution
+        if column<0
+            fprintf('Unbounded solution\n');
+        else
+            for i=1:size(A,1) %for every value in pivot column
+                if column(i)>0 %if positive then get ratio
+                    ratio(i) = sol(i)./column(i);
+                else  %else store as inf
+                    ratio(i) = inf;
+                end
+            end
+            %find the minimum ratio
+            [minRatio,pvt_row] = min(ratio);
+            fprintf('pivot row is : %d\n',pvt_row);
+        end
+        %update basic variables
+        BV(pvt_row)  = pvt_col;
+        %set pivot element
+        pvt_key = A(pvt_row,pvt_col);
+        %set pvt_key as 1 and all other entries in column as 0
+        A(pvt_row,:) = A(pvt_row,:)./pvt_key; %divide entire row by pvtkey
+        for i = 1:size(A,1)   %for all rows other than pivot row
+            if i~=pvt_row
+                A(i,:) = A(i,:) - A(i,pvt_col).*A(pvt_row,:);  %make element as zero
             end
         end
-        %find the minimum ratio
-        [minRatio,pvt_row] = min(ratio);
-        fprintf('pivot row is : %d\n',pvt_row);
+        %updating the Zj - Cj row
+        ZjCj = ZjCj - ZjCj(pvt_col).*A(pvt_row,:);
+        %print the table
+        table = array2table([ZjCj;A]); 
+        table.Properties.VariableNames(1:size(A,2))=variables
+        %print the BFS
+        BFS(BV) = A(:,end);
+
+    else %if no ZjCj is negative then solution is reached
+        RUN = false;
+        fprintf('optimal solution is reached for phase one\n');
+        BFS = BV;
     end
-    %update basic variables
-    BV(pvt_row)  = pvt_col;
-    %set pivot element
-    pvt_key = A(pvt_row,pvt_col);
-    %set pvt_key as 1 and all other entries in column as 0
-    A(pvt_row,:) = A(pvt_row,:)./pvt_key; %divide entire row by pvtkey
-    for i = 1:size(A,1)   %for all rows other than pivot row
-        if i~=pvt_row
-            A(i,:) = A(i,:) - A(i,pvt_col).*A(pvt_row,:);  %make element as zero
-        end
-    end
-    %updating the Zj - Cj row
-    ZjCj = ZjCj - ZjCj(pvt_col).*A(pvt_row,:);
-    %print the table
-    table = array2table([ZjCj;A]); 
-    table.Properties.VariableNames(1:size(A,2))=variables
-    %print the BFS
-    BFS(BV) = A(:,end);
-    
-else %if no ZjCj is negative then solution is reached
-    RUN = false;
-    fprintf('optimal solution is reached for phase one\n');
-    BFS = BV;
-end
 end
 
 fprintf('PHASE 2 STARTS\n');
